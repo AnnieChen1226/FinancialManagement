c077ba8ff451445ea520ea57c0278cf15e88d026
AnnieChen1226 <52643694+AnnieChen1226@users.noreply.github.com>
2021-01-08 05:52:56 +0800

    Initial commit

---
+++ testDataFinal.R
@@ -0,0 +1,82 @@
+library(readxl)
+library(dplyr)
+
+
+data4block.df= read.csv("D:\\test\\廣達\\2382.csv", sep=",", row.names=1)
+names(data4block.df)
+temp1 <- as.matrix(data4block.df) 
+temp2<-c("date","k","d","rsv","sol")
+head(data4block)
+i<-1
+datelist<-c("date")
+klist<-c("k")
+dlist<-c("d")
+rsvlist<-c("rsv")
+sollist<-c("sol")
+while(i<955)
+{
+TureORFalse1<-temp1[i,6]
+TureORFalse2<-temp1[i+1,6]
+date<-temp1[i,1]
+k<-temp1[i,2]
+d<-temp1[i,3]
+rsv<-temp1[i,4]
+sol<-''
+mylist <- c(date,k,d,rsv,sol)
+
+
+if(TureORFalse1!=TureORFalse2)
+{
+  if(TureORFalse1==1)
+  {
+    sol<-"death"
+    print(date)
+    print(k)
+    print(d)
+    print(rsv)
+    print(sol)
+    
+    datelist <- c(datelist, date)
+    klist <- c(klist, k)
+    dlist <- c(dlist, d)
+    rsvlist <- c(rsvlist, rsv)
+    sollist <- c(sollist, sol)
+    
+    print("--------")
+  }
+  else{
+    sol<-"golden"
+    print(date)
+    print(k)
+    print(d)
+    print(rsv)
+    print(sol)
+    
+    datelist <- c(datelist, date)
+    klist <- c(klist, k)
+    dlist <- c(dlist, d)
+    rsvlist <- c(rsvlist, rsv)
+    sollist <- c(sollist, sol)
+    
+    
+    print("--------")
+  }
+}
+else
+{
+  print(temp1[i,1])
+  print("nothing change")
+  print("--------")
+}
+
+
+i<-i+1
+}
+
+Total<-cbind(datelist,klist,dlist,rsvlist,sollist)
+print(Total)
+write.csv(Total,file="D:\\test\\廣達\\2382text.csv",row.names = FALSE)
+rm(Total)
+rm(data4block.df)
+rm(temp1)
+rm(temp2)
